51a846d5819db0eebf08d18012acf7abcc07ecfe
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2025-08-23 10:20:24 -0600

    added a new button for recharge balance

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
 import { AppKitButton } from '@reown/appkit-wagmi-react-native';
 import { Image } from 'expo-image';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { router } from 'expo-router';
 
 export default function HomeScreen() {
   return (
@@ -25,7 +26,23 @@ export default function HomeScreen() {
           <ThemedText type="title">¡Bienvenido!</ThemedText>
           {/* <HelloWave /> */}
         </ThemedView>
-        <AppKitButton connectStyle={styles.appKitButton} label='Iniciar sesión' />
+        <AppKitButton
+          connectStyle={styles.appKitButton}
+          label='Iniciar sesión con billetera'
+          size='md'
+          loadingLabel='Conectando...'
+        />
+        <ThemedText type="title">Saldo: $1,000.00</ThemedText>
+
+        <Pressable
+          style={styles.button}
+          onPress={() => {
+            console.log('Next button pressed');
+            router.push('./payment-receiver-confirmation');
+          }}
+        >
+          <ThemedText style={styles.buttonText}>Recargar saldo</ThemedText>
+        </Pressable>
       </ParallaxScrollView>
 
     </>
@@ -50,5 +67,33 @@ const styles = StyleSheet.create({
   },
   appKitButton: {
     marginTop: 20,
+    marginBottom: 20,
+    height: 90,
+    backgroundColor: '#9D4EDD',
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 32,
+  },
+  button: {
+    backgroundColor: '#9D4EDD',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 20,
+    marginTop: 16,
+    height: 100,
+    width: '100%',
+    textAlign: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 24,
+    textAlign: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
